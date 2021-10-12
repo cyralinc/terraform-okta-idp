d8979e747d55adfe574a5edc62c301c3ead0ae2e
Victor Moraes <vmoraes@cyral.com>
2021-10-12 18:54:26 -0300

    Updates module with new saml integration resource

--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
+variable "okta_app_name" {
+  type = string
+  description = "The name of the app that will be created in Okta."
+}
+
+variable "okta_groups" {
+  type = list(string)
+  description = "Groups that the cyral app will be assigned to in Okta."
+  default = []
+}
+
+variable "integration_name" {
+  type = string
+  description = "Integration name that will be shown in Control Plane."
+}
+
 variable "tenant" {
   type = string
   description = "Tenant associated with the control plane"
@@ -7,20 +23,4 @@ variable "tenant" {
 variable "control_plane" {
   type = string
   description = "Control plane host and API port (ex: some-cp.cyral.com:8000)"
-}
-
-variable "integration_name" {
-  type = string
-  description = "Integration name that will be shown in Control Plane."
-}
-
-variable "okta_groups" {
-  type = list(string)
-  description = "Groups that the cyral app will be assigned to in Okta."
-  default = []
-}
-
-variable "okta_app_name" {
-  type = string
-  description = "The name of the app that will be created in Okta."
 }
